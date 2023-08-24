c583fa820adcecd603a6e1824198cfe785103fae
hongmoSung <tesung30@naver.com>
2023-08-24 14:00:09 +0900

    feat: 서블릿 컨테이너 띄우기

---
+++ docs/containerless.md
@@ -0,0 +1,11 @@
+## Containerless
+
+컨테이너 웹 애플리케이션 아키텍쳐
+
+### Container
+
+Web Container(Java: Servlet Conatiner) -> Web Component를 관리 -> life cycle 관리
+
+Web Component(Java: Servlet) -> Dynamic Content를 만듦
+
+Web Client -> Web Request
--- src/main/java/com/helloboot/HellobootApplication.java
+++ src/main/java/com/helloboot/HellobootApplication.java
@@ -1,13 +1,15 @@
 package com.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
-@SpringBootApplication
 public class HellobootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer();
+        webServer.start();
+    }
 
 }
