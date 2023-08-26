749124161d28b28ee9196ec822c36435bc81f8b7
hongmoSung <tesung30@naver.com>
2023-08-26 17:08:33 +0900

    feat: 프론트 컨트롤러로 전환

--- src/main/java/com/helloboot/HellobootApplication.java
+++ src/main/java/com/helloboot/HellobootApplication.java
@@ -1,6 +1,5 @@
 package com.helloboot;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -8,6 +7,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -18,14 +18,22 @@ public class HellobootApplication {
     public static void main(String[] args) {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext ->
-                servletContext.addServlet("hello", new HttpServlet() {
+                servletContext.addServlet("helloController", new HttpServlet() {
                     @Override
-                    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                        String name = req.getParameter("name");
-                        resp.setStatus(HttpStatus.OK.value());
-                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                        resp.getWriter().println("Hello " + name);
-                        resp.getWriter().println("Nice to meet you");
+                    protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+                        // 인증, 보안, 다국어..
+                        if (req.getRequestURI().equals("/hello")
+                                && req.getMethod().equals(HttpMethod.GET.name())) {
+                            String name = req.getParameter("name");
+                            resp.setStatus(HttpStatus.OK.value());
+                            resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                            resp.getWriter().println("Hello " + name);
+                            resp.getWriter().println("Nice to meet you");
+                        } else if (req.getRequestURI().equals("users")) {
+                            //
+                        } else {
+                            resp.setStatus(HttpStatus.NOT_FOUND.value());
+                        }
                     }
                 }).addMapping("/hello")
         );
