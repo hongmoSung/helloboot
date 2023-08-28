c676bc61c5e4c70b75698d9dff18344ff017f2cb
hongmoSung <tesung30@naver.com>
2023-08-29 00:15:46 +0900

    feat: @Component 스캔

--- src/main/java/com/helloboot/SimpleHelloService.java
+++ src/main/java/com/helloboot/SimpleHelloService.java
@@ -1,5 +1,8 @@
 package com.helloboot;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class SimpleHelloService implements HelloService {
     @Override
     public String sayHello(String name) {
