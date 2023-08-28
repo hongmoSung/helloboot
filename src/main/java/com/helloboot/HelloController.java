648722d1eb3c741e03098e21a578518f58a423c0
hongmoSung <tesung30@naver.com>
2023-08-29 00:38:49 +0900

    feat: Bean의 생명주기 메소드

--- src/main/java/com/helloboot/HelloController.java
+++ src/main/java/com/helloboot/HelloController.java
@@ -1,5 +1,6 @@
 package com.helloboot;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +10,13 @@ import java.util.Objects;
 public class HelloController {
 
     private final HelloService helloService;
+    private final ApplicationContext applicationContext;
 
-    public HelloController(HelloService helloService) {
+    public HelloController(HelloService helloService, ApplicationContext applicationContext) {
         this.helloService = helloService;
+        this.applicationContext = applicationContext;
+
+        System.out.println("applicationContext = " + applicationContext);
     }
 
     @GetMapping("/hello")
--- src/main/java/com/helloboot/HellobootApplication.java
+++ src/main/java/com/helloboot/HellobootApplication.java
@@ -3,6 +3,7 @@ package com.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -12,16 +13,29 @@ import org.springframework.web.servlet.DispatcherServlet;
 @Configuration
 public class HellobootApplication {
 
+    @Bean
+    public ServletWebServerFactory webServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
     public static void main(String[] args) {
         // 스프링 컨테이너
         AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
             @Override
             protected void onRefresh() {
                 super.onRefresh();
-                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+
+                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+//                dispatcherServlet.setApplicationContext(this); 이걸 안해도 동작하는게 신기해야함
+
                 WebServer webServer = serverFactory.getWebServer(servletContext ->
-                        servletContext.addServlet("dispatcherServlet",
-                                        new DispatcherServlet(this))
+                        servletContext.addServlet("dispatcherServlet", dispatcherServlet)
                                 .addMapping("/*")
                 );
                 webServer.start();
