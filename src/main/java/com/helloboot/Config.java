d1eae51b0b344eac951d5e23c73accb215ad448a
hongmoSung <tesung30@naver.com>
2023-09-03 13:55:02 +0900

    feat: 합성 애노테이션의 적용

---
+++ src/main/java/com/helloboot/Config.java
@@ -0,0 +1,21 @@
+package com.helloboot;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public ServletWebServerFactory webServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+}
--- src/main/java/com/helloboot/HellobootApplication.java
+++ src/main/java/com/helloboot/HellobootApplication.java
@@ -1,26 +1,9 @@
 package com.helloboot;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.DispatcherServlet;
 
-@ComponentScan
-@Configuration
+@MySpringBootApplication
 public class HellobootApplication {
-
-    @Bean
-    public ServletWebServerFactory webServerFactory() {
-        return new TomcatServletWebServerFactory();
-    }
-
-    @Bean
-    public DispatcherServlet dispatcherServlet() {
-        return new DispatcherServlet();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(HellobootApplication.class, args);
