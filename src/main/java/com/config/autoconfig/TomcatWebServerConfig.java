2d212ebc69ef0b2cdd7b26d059995db1d8ffd331
hongmoSung <tesung30@naver.com>
2023-09-09 17:08:03 +0900

    feat: @Conditional 학습테스트

--- src/main/java/com/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/com/config/autoconfig/TomcatWebServerConfig.java
@@ -4,12 +4,24 @@ import com.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @MyAutoConfiguration
+@Conditional(TomcatWebServerConfig.TomcatCondition.class)
 public class TomcatWebServerConfig {
 
     @Bean("tomcatWebServerFactory")
     public ServletWebServerFactory webServerFactory() {
         return new TomcatServletWebServerFactory();
+    }
+
+    static class TomcatCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return false;
+        }
     }
 }
