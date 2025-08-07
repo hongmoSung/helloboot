c1d941b60ac5530a36364c8d9d1c83a1d47779bb
hongmoSung <tesung30@naver.com>
2025-08-07 20:49:13 +0900

    feat: 스프링 컨테이너 사용

--- src/main/java/com/helloboot/HellobootApplication.java
+++ src/main/java/com/helloboot/HellobootApplication.java
@@ -7,7 +7,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.http.HttpHeaders;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -16,10 +16,13 @@ import java.io.IOException;
 public class HellobootApplication {
 
 	public static void main(String[] args) {
+
+        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        applicationContext.registerBean(HelloController.class);
+        applicationContext.refresh();
+
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-
-            HelloController helloController = new HelloController();
 
             servletContext.addServlet("frontController", new HttpServlet() {
                 @Override
@@ -29,12 +32,11 @@ public class HellobootApplication {
                     if (url.equals("/hello") && req.getMethod().equals("GET")) {
                         String name = req.getParameter("name");
 
+                        HelloController helloController = applicationContext.getBean(HelloController.class);
                         String ret = helloController.hello(name);
 
-                        resp.setStatus(HttpStatus.OK.value());
-                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
                         resp.getWriter().println(ret);
-                    } else if (url.equals("/users")) {
 
                     } else {
                         resp.setStatus(HttpStatus.NOT_FOUND.value());
