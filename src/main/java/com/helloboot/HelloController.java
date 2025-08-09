64d30d5052256b02280fec59f928756bd057c40e
hongmoSung <tesung30@naver.com>
2025-08-09 18:10:10 +0900

    feat: 애노테이션 매 정보 사용

--- src/main/java/com/helloboot/HelloController.java
+++ src/main/java/com/helloboot/HelloController.java
@@ -1,5 +1,12 @@
 package com.helloboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
 public class HelloController {
 
     private final HelloService helloService;
@@ -8,6 +15,7 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping
     public String hello(String name) {
         return helloService.sayHello(name);
     }
--- src/main/java/com/helloboot/HellobootApplication.java
+++ src/main/java/com/helloboot/HellobootApplication.java
@@ -1,19 +1,10 @@
 package com.helloboot;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-
-import java.io.IOException;
 
 public class HellobootApplication {
 
